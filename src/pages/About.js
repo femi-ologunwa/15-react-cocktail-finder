afdfab9ba66bc1d4947d7f99897d9e9da9483748
Femi <ologunwafemi@gmail.com>
2021-05-14 11:18:59 +0100

    built the details of about & error pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
-  return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+   return (
+      <section className='section about-section'>
+         <h1 className='section-title'>about us</h1>
+         <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores,
+            in culpa ducimus mollitia aliquid odit, voluptate enim,
+            exercitationem sit tempore rerum. Voluptatem consectetur cumque amet
+            aut est voluptate harum facere blanditiis inventore? Aliquid
+            similique consectetur eum dignissimos optio aliquam dolor vitae, ab
+            ipsa nulla, accusantium quam, molestias blanditiis fugiat fuga.
+         </p>
+      </section>
+   );
+};
 
-export default About
+export default About;
