a1ba2932b70de5c4cf905b2cfd85e5ca8469edc2
Femi <ologunwafemi@gmail.com>
2021-05-15 12:41:35 +0100

    built the details of the single cocktail page - 2
    render fetched data

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -65,10 +65,53 @@ const SingleCocktail = () => {
       getCocktail();
    }, [id]);
 
+   if (loading) {
+      return <Loading />;
+   }
+
+   if (!cocktail) {
+      return <h2 className='section-title'>no cocktail to display</h2>;
+   }
+
+   const { name, image, category, info, glass, instructions, ingredients } =
+      cocktail;
    return (
-      <div>
-         <h2>{id}</h2>
-      </div>
+      <section className='section cocktail-section'>
+         <Link to='/' className='btn btn-primary'>
+            back home
+         </Link>
+         <h2 className='section-title'>{name}</h2>
+         <div className='drink'>
+            <img src={image} alt={name} />
+            <div className='drink-info'>
+               <p>
+                  <span className='drink-data'>name :</span> {name}
+               </p>
+               <p>
+                  <span className='drink-data'>category : </span> {category}
+               </p>
+               <p>
+                  <span className='drink-data'>info : </span> {info}
+               </p>
+               <p>
+                  <span className='drink-data'>glass :</span> {glass}
+               </p>
+               <p>
+                  <span className='drink-data'>instructions :</span>{' '}
+                  {instructions}
+               </p>
+               <p>
+                  <span className='drink-data'>ingredients :</span>{' '}
+                  {ingredients.map((ingredient, index) => {
+                     //check if the fetched ingredient is not null
+                     return ingredient ? (
+                        <span key={index}>{ingredient}</span>
+                     ) : null;
+                  })}
+               </p>
+            </div>
+         </div>
+      </section>
    );
 };
 
