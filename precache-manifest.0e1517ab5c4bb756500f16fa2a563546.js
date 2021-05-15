fded717e5fae173a9014c2cb2858c830f6c43b59
Femi <ologunwafemi@gmail.com>
2021-05-15 13:39:08 +0100

    Updates

---
+++ precache-manifest.0e1517ab5c4bb756500f16fa2a563546.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fa4d796ba2ac19a2f3b784eb0fa75af",
+    "url": "/15-react-cocktail-finder/index.html"
+  },
+  {
+    "revision": "303e3311c7113e8f86ca",
+    "url": "/15-react-cocktail-finder/static/css/main.5c529f91.chunk.css"
+  },
+  {
+    "revision": "b2ddc5554eefa67ccb0a",
+    "url": "/15-react-cocktail-finder/static/js/2.975d57c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/15-react-cocktail-finder/static/js/2.975d57c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "303e3311c7113e8f86ca",
+    "url": "/15-react-cocktail-finder/static/js/main.135e1b2d.chunk.js"
+  },
+  {
+    "revision": "5b3c6099bd01653b11bb",
+    "url": "/15-react-cocktail-finder/static/js/runtime-main.1d6c7755.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/15-react-cocktail-finder/static/media/logo.9a3d2645.svg"
+  }
+]);
